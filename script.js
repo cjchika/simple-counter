f5a958d56c36df3a8934d69625daaf578329ec0b
Chijioke Chika <8pluscreative@gmail.com>
2022-05-13 13:42:49 -0400

    Updating

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 const incrementBtn = document.getElementById("increment-btn")
 const saveBtn = document.getElementById("save-btn")
 let counterEl = document.getElementById("counter-el")
-let welcomeText = document.getElementById("welcome-el")
+let saveText = document.getElementById("save-el")
 
 
 let count = 0;
 
 function increment(){
-    count = count + 1;
+    count += 1;
     counterEl.innerText = count;
 }
 
 incrementBtn.addEventListener("click", increment)
 
 function save(){
-    console.log(count);
+    let displayText = count + " - "
+    saveText.textContent += displayText; 
+    counterEl.textContent = 0;
 }
+
 
 saveBtn.addEventListener("click", save)
 
-let name = "Odogwu"
 
-let greeting = "Welcome back";
-
-welcomeText.innerText = `${greeting}, ${name}!`
